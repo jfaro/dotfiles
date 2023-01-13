1f50346ca3d49d0ccb23eecab3a28f36d0e64a0b
Julien Faro <jfaro@umich.edu>
2023-01-12 21:39:08 -0500

    Updates to fish and i3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,15 @@ end
 
 set fish_greeting
 
+# Common command aliases
+alias c="clear"
+
 # Git aliases
+alias gco="git checkout"
 alias gb="git branch"
+alias gd="git diff"
 alias gst="git status"
 alias ga="git add"
 alias gcmsg="git commit -m"
+
+alias extend="xrandr --output eDP-1 --auto --output DP-1 --auto --left-of eDP-1"
