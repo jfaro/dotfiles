2e9e6d6bb99a9da2c448c792e2ebb00bb69d772f
Julien Faro <jfaro@umich.edu>
2023-02-25 01:41:42 -0500

    Update theming

--- update.sh
+++ update.sh
@@ -2,10 +2,26 @@
 
 #
 # .config files
-cp -r ~/.config/fish/ ./.config/                            # fish
-cp -r ~/.config/i3/ ./.config/                              # i3
-cp -r ~/.config/i3status/ ./.config/                        # i3 statusbar
-cp -r ~/.config/kitty/ ./.config/                     # kitty
+CONFIG_DIRS="fish i3 i3status kitty terminator rofi gtk-3.0"
+
+function copy_config_from_dir_to() {
+    local from="$2/$1/"
+    local to="$3/"
+
+    if [ -d $from ]
+    then
+        # Make copy of config
+        cp -r $from $to
+    else
+        echo "error: $from does not exist"
+    fi
+}
+
+# Backup all configs in CONFIG_DIRS
+for cfg in $CONFIG_DIRS
+do
+    copy_config_from_dir_to $cfg "$HOME/.config" "./.config"
+done
 
 #
 # etc files
